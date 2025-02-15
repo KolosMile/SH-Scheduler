ba314ab603031fe58a8a20271d877251b7c23082
Kolos Mile <kolosk5@gmail.com>
2025-02-15 17:31:18 +0100

    Remove automatic JSON file push from sync script

--- sync.sh
+++ sync.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Pushing JSON file to GitHub..."
-git add missed_streak.json
-git commit -m "Auto-update missed_streak.json from server"
-git push origin main
-
 echo "Pulling latest code from GitHub..."
 git pull origin main
 
